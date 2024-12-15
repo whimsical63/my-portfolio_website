562dbcee0077e921a99d3c953dbf93aa430d63be
L A <laurenceunabia@gmail.com>
2024-12-15 19:20:04 +0800

    update projects section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -22,7 +22,7 @@ const Projects = () => {
     }
     return (
         <section className="c-space my-20" id="work">
-            <p className="head-text">My Work</p>
+            <p className="head-text">My Projects</p>
 
             <div className="grid lg:grid-cols-2
             grid-cols-1 mt-12 gap-5 w-full">
